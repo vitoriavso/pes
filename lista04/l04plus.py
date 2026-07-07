eb1d4f156962a27f1143ad3b20a6f53ba499faca
Vitoria Ortiz <vitoria.vso@aluno.ifsc.edu.br>
2026-07-07 20:08:40 +0000

    lista 4 plus

---
+++ lista04/l04plus.py
@@ -0,0 +1,62 @@
+'''1 - Nossa necessidade é utilizar o recurso de liberação de portas dos Laboratórios de
+Informática utilizando os dispositivos instalados em cada porta com fechadura eletrônica.
+Para tal, desenvolveremos um sistema que identifique e autorize a entrada dos
+professores já cadastrados no sistema de uso dos laboratórios.
+
+Implemente um algoritmo de acordo com os requisitos listados abaixo:
+A – Serão utilizados 6 laboratórios com as nomenclaturas Lab102, Lab103, Lab104,
+Lab105, Lab106, Lab107.
+
+B – Os professores autorizados de acordo com cada laboratório:
+• Lab102 – Prof Ignácio, Prof Thiago Paes, Prof Ryan, Prof André, Profª
+Fabiana;
+• Lab103 – Prof Alberto;
+• Lab104 – Prof Ryan, Prof Juliano, Prof Schalata, Prof André;
+• Lab105 – Prof Ignácio, Prof Alberto, Prof Thiago Waltrik, Prof Thiago Paes;
+• Lab106 – Prof Schalata, Prof Ignácio, Prof Thiago Waltrik, Prof Thiago Paes;
+• Lab107 – Prof André, Prof Schalata, Prof Thiago Waltrik, Prof Thiago Paes, Prof
+João Eduardo.
+
+C – O programa deverá ter uma opção para cadastrar as listas por laboratório e a
+lista de professores. Deverá ter opção de imprimir todos os cadastros de cada laboratório,
+bem como a lista de professores com seu código de crachá.
+
+D - O sistema de identificação de liberação da porta será feito através do fecho
+eletrônico através do Crachá do Servidor, conforme a tabela de crachás abaixo. Vamos
+simular que a autorização se dará por meio do teclado dos dispositivos na porta. Você
+deve criar no seu programa, uma chamada que permita digitar o código do crachá.
+
+Cadastro de Professores
+001 – Prof Thiago Paes
+002 – Prof Schalata
+003 – Prof Ignácio
+004 – Prof Ryan
+005 – Prof André
+006 – Profª Fabiana
+007 – Prof Alberto
+008 – Prof Juliano
+009 – Prof Thiago Waltrik
+010 – Prof João Eduardo
+
+Observação: Faça com que seu algoritmo “converse” com o usuário e que seja claro
+na troca de mensagens.'''
+
+print('\n', 'Liberação dos Laboratórios', '\n')
+
+profs = {'001' : "Prof Thiago Paes",
+'002' : "Prof Schalata",
+'003' : "Prof Ignácio",
+'004' : "Prof Ryan",
+'005' : "Prof André",
+'006' : "Profª Fabiana",
+'007' : "Prof Alberto",
+'008' : "Prof Juliano",
+'009' : "Prof Thiago Waltrik",
+'010' : "Prof João Eduardo"}
+
+lab102 = ['001', '003', '004', '005', '006']
+lab103 = ['007']
+lab104 = ['004', '008', '002', '005']
+lab105 = ['001', '003', '009', '007']
+lab106 = ['001', '009', '003', '002']
+lab107 = ['001', '002', '009', '005']
